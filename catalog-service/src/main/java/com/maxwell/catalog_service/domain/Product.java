67fef991444117001348ccd455266011cc6a3ed2
maxwell-balla <ballamaxwell7@gmail.com>
2024-11-24 07:12:43 +0100

    update serializable on product dto

--- catalog-service/src/main/java/com/maxwell/catalog_service/domain/Product.java
+++ catalog-service/src/main/java/com/maxwell/catalog_service/domain/Product.java
@@ -1,5 +1,7 @@
 package com.maxwell.catalog_service.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,11 +14,21 @@ import java.math.BigDecimal;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Product implements Serializable {
 
+    @JsonProperty("code")
     private String code;
+
+    @JsonProperty("name")
     private String name;
+
+    @JsonProperty("description")
     private String description;
+
+    @JsonProperty("imageUrl")
     private String imageUrl;
+
+    @JsonProperty("price")
     private BigDecimal price;
 }
